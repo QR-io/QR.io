2b7b8883e5fd536562da6003eaf795f33bed6bde
tpr <tom.p.reichel@gmail.com>
2020-02-28 17:53:14 -0600

    .

--- app/src/main/java/com/example/snappyqr/MainActivity.kt
+++ app/src/main/java/com/example/snappyqr/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.snappyqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bcd = BarcodeDetector.Builder(applicationContext).setBarcodeFormats(Barcode.QR_CODE)
 
     }
 }
