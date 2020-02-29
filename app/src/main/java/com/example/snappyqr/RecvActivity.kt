cc2237b11b1395061f18107622dd21d161269e78
Matas Lauzadis <matas.lauzadis@gmail.com>
2020-02-29 14:47:43 -0600

    Added ImageView to RecvActivity

--- app/src/main/java/com/example/snappyqr/RecvActivity.kt
+++ app/src/main/java/com/example/snappyqr/RecvActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.TextureView
+import android.widget.Button
+import android.widget.ImageView
 import com.google.android.gms.vision.Frame
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
@@ -18,8 +20,21 @@ class RecvActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_recv)
 
-        Routines.setupCamViewer(this,findViewById(R.id.textureView))
+        Routines.setupCamViewer(this, findViewById(R.id.textureView))
+
+        val qr_image = findViewById<ImageView>(R.id.qr_image)
+
+        val imgResId = R.drawable.ic_launcher_background
+        var resId = imgResId
+        qr_image.setImageResource(imgResId)
+
+        val button = findViewById<Button>(R.id.switch_image)
+        button?.setOnClickListener {
+            resId = if (resId == R.drawable.ic_launcher_background) R.mipmap.ic_launcher else R.drawable.ic_launcher_background
+            qr_image.setImageResource(resId)
+        }
     }
+}
 
 /*
  {
@@ -29,4 +44,3 @@ class RecvActivity : AppCompatActivity() {
  }
  */
 
-}
