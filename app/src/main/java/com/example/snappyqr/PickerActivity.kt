85bb5ed33801bfbb6bf7f70aa5a3076b1f2af8c0
Trevato <trevordobbertin@gmail.com>
2020-02-29 02:49:04 -0600

    Added file search.

--- app/src/main/java/com/example/snappyqr/PickerActivity.kt
+++ app/src/main/java/com/example/snappyqr/PickerActivity.kt
@@ -1,23 +1,22 @@
 package com.example.snappyqr
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class PickerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picker)
-    }
-    fun onClick(view: View){
-        //file picker code
-
-    }
-    fun showFileChooser(){
-        val intent = Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select a File"), 0);
-
+        val chooseFileButton = findViewById<Button>(R.id.choose_file_button)
+        chooseFileButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "file/*";
+            startActivityForResult(intent, 1)
+        }
     }
 }
