109aa8814870b0992f6ba73014cd7921a4b59ac4
tpr <tom.p.reichel@gmail.com>
2020-02-29 15:13:57 -0600

    Permissions seem to work completely, informs users when something is wrong.

--- app/src/main/java/com/example/snappyqr/MainActivity.kt
+++ app/src/main/java/com/example/snappyqr/MainActivity.kt
@@ -10,7 +10,9 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 //import com.google.android.gms.vision.barcode.Barcode
@@ -18,21 +20,31 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.send_button).setOnClickListener{
+            startActivity(Intent(this, PickerActivity::class.java))
+        }
+        findViewById<Button>(R.id.receive_button).setOnClickListener{
+            startActivity(Intent(this, RecvActivity::class.java))
+        }
+
 
         askForPermissions(Manifest.permission.CAMERA)
         askForPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
 
     }
 
-    val REQUEST_CODE = 77
 
     // permission handling code taken with credit to https://handyopinion.com/ask-runtime-permission-in-kotlin-android/
     fun isPermissionsAllowed(perm:String): Boolean {
         return ContextCompat.checkSelfPermission(this,perm) == PackageManager.PERMISSION_GRANTED
     }
+
+    private val REQUEST_CODE = 8
 
     fun askForPermissions(perm:String): Boolean {
         if (!isPermissionsAllowed(perm)) {
@@ -47,25 +59,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>,grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_CODE -> {
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission is granted, you can perform your operation here
-                    findViewById<Button>(R.id.send_button).setOnClickListener{
-                        startActivity(Intent(this, PickerActivity::class.java))
-                    }
-                    findViewById<Button>(R.id.receive_button).setOnClickListener{
-                        startActivity(Intent(this, RecvActivity::class.java))
-                    }
-                } else {
-                    // permission is denied, you can ask for permission again, if you want
-                    for(perm in permissions){
-                        if (!isPermissionsAllowed(perm)){
-                            askForPermissions(perm)
-                        }
-                    }
-                }
-                return
+        //PackageManager.permission_granted
+        for (x in grantResults) {
+            if (x!=PackageManager.PERMISSION_GRANTED) {
+                // UH-OH. At least one permission we requested was not granted. We'll disable the buttons.
+                disableButtons()
+
+
             }
         }
     }
@@ -85,6 +85,17 @@ class MainActivity : AppCompatActivity() {
                 })
             .setNegativeButton("Cancel",null)
             .show()
+
+        // We didn't get the perms, so we have to disable the button until the user comes back.
+        disableButtons()
     }
 
+    fun disableButtons() {
+        val toastRestart = {v: View ->
+            Toast.makeText(applicationContext, "Please grant the app permissions in settings and restart the app.",
+                Toast.LENGTH_LONG).show()
+        }
+        findViewById<Button>(R.id.send_button).setOnClickListener(toastRestart)
+        findViewById<Button>(R.id.receive_button).setOnClickListener(toastRestart)
+    }
 }
