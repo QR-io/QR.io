0c869bb893a8c5bb6d09ddc8dc8c9b39b38d3bd7
Trevato <trevordobbertin@gmail.com>
2020-02-29 16:42:34 -0600

    Create QR codes test QR codes.
    QR codes are now created with a test string.
    The URI of the picked file is also sent to the send activity.

--- app/src/main/java/com/example/snappyqr/SendActivity.kt
+++ app/src/main/java/com/example/snappyqr/SendActivity.kt
@@ -1,12 +1,23 @@
 package com.example.snappyqr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import net.glxn.qrgen.android.QRCode
+
 
 class SendActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send)
+        val qr_string = intent.getStringExtra("qr_string")
+        val uri = intent.getStringExtra("uri")
+
+        val myBitmap: Bitmap = QRCode.from(qr_string).bitmap()
+        val myImage: ImageView = findViewById<View>(R.id.imageView) as ImageView
+        myImage.setImageBitmap(myBitmap)
     }
 }
