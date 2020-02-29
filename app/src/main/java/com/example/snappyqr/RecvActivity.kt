d09424d9e63dc18fae535ba534264e90820f20dc
tpr <tom.p.reichel@gmail.com>
2020-02-29 09:04:41 -0600

    camera demo

--- app/src/main/java/com/example/snappyqr/RecvActivity.kt
+++ app/src/main/java/com/example/snappyqr/RecvActivity.kt
@@ -1,12 +1,57 @@
 package com.example.snappyqr
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import android.view.ViewGroup
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import com.example.snappyqr.Routines
+import kotlinx.android.synthetic.main.activity_recv.*
 
 class RecvActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_recv)
+        demo()
     }
+
+    fun demo() {
+        // modified example code from https://codelabs.developers.google.com/codelabs/camerax-getting-started/
+
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = textureView.parent as ViewGroup
+            parent.removeView(textureView)
+            parent.addView(textureView, 0)
+
+            textureView.surfaceTexture = it.surfaceTexture
+            //updateTransform()
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+
 }
--- app/src/main/java/com/example/snappyqr/Routines.kt
+++ app/src/main/java/com/example/snappyqr/Routines.kt
@@ -1,15 +1,26 @@
 package com.example.snappyqr
 
-import android.hardware.Camera
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.security.AccessController.getContext
 
 
 class Routines {
-/*
-    fun start() {
-        val cam = Camera.open(0)
-        cam.
+
+    companion object {
+        fun checkCameraPerms(c: Context,a: Activity) {
+            if (ContextCompat.checkSelfPermission(
+                    c,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Let's grab permission.
+                ActivityCompat.requestPermissions(a, arrayOf(Manifest.permission.CAMERA), 1234)
+            }
+        }
     }
-*/
-
-
 }
