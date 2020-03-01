c5ac55417379ca2e96dae60c5247011883165e4d
tpr <tom.p.reichel@gmail.com>
2020-02-29 19:18:51 -0600

    demo sending also added frame listener for receiving

--- app/src/main/java/com/example/snappyqr/SendActivity.kt
+++ app/src/main/java/com/example/snappyqr/SendActivity.kt
@@ -1,23 +1,46 @@
 package com.example.snappyqr
 
-import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import net.glxn.qrgen.android.QRCode
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 
 class SendActivity : AppCompatActivity() {
 
+    val fakeData : ByteArray = ByteArray(1000)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        for (x in 0..fakeData.size-1) {
+            fakeData[x] = x.toByte()
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send)
         val qr_string = intent.getStringExtra("qr_string")
         val uri = intent.getStringExtra("uri")
 
+        /*
         val myBitmap: Bitmap = QRCode.from(qr_string).bitmap()
         val myImage: ImageView = findViewById<View>(R.id.imageView) as ImageView
         myImage.setImageBitmap(myBitmap)
+
+         */
+        var counter = 0
+        val myImage: ImageView = findViewById<View>(R.id.imageView) as ImageView
+        val flashqrs : () -> Unit = {
+            counter+=1
+            counter = counter.rem(99)
+            myImage.setImageBitmap(Routines.getNthQRCode(counter,fakeData,5))
+            //Log.w("SnappyQR","Thread running.")
+        }
+        val exec = Executors.newSingleThreadScheduledExecutor()
+        exec.scheduleAtFixedRate(flashqrs,100,100, TimeUnit.MILLISECONDS)
+        //exec.
+
     }
 }
